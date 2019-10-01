228e363d15a118f9bc6bf6d3019f71d86dce01a1
cjste <cjste@192.168.254.75>
2019-10-01 17:16:25 -0500

    Created the createExam Method

--- model/exam.php
+++ model/exam.php
@@ -1,9 +1,10 @@
 <?php
+include_once 'model/question';
 class exam {
-    private $questions,$level,$time;
+    private $questions = [],$level,$time,$questionQuantity;
     
-    function __construct($questions, $level, $time) {
-        $this->questoions = $questions;
+    function __construct($questionQuantity, $level, $time) {
+        $this->questoionQuantity = $questionQuantity;
         $this->level= $level;
         $this->time = $time;
     }
@@ -30,5 +31,35 @@ class exam {
 
     function setTime($time) {
         $this->time = $$time;
+    }
+    
+    function getQuestionQuantity() {
+        return $this->questionQuantity;
+    }
+
+    function setQuestionQuantity($questionQuantity) {
+        $this->questionQuantity = $questionQuantity;
+    }
+    
+    public static function createExam($lvl){
+        if($lvl === "m"){
+            $levels = 1;
+            $count = 0;
+            while(level <= 11){
+                while($count <5){;
+                    $question = new question();
+                    $questions[$questions.length] = $newQuestion;
+                    $count++;
+                }
+                $levels++;
+            }
+        }
+        else{
+            while($count < $this->questionQuantity){
+                $question = new question($this->level);
+                $questions[$questions.length] = $newQuestion;
+                $count++;
+            }
+        }
     }
 }
