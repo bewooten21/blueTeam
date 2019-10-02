39814722d42077a882719db7bca171a884004875
bw447997 <bw447997@LR00112.southeast.edu>
2019-10-02 13:28:21 -0500

    role table

--- teamblue.sql
+++ teamblue.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2019 at 09:19 PM
+-- Generation Time: Oct 02, 2019 at 08:27 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `teamblue`
 --
-CREATE DATABASE IF NOT EXISTS `teamblue` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `teamblue`;
 
 -- --------------------------------------------------------
 
@@ -66,6 +64,26 @@ CREATE TABLE `question` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'teacher'),
+(3, 'student');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -75,18 +93,19 @@ CREATE TABLE `user` (
   `lastName` varchar(40) NOT NULL,
   `userName` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `level` int(11) DEFAULT NULL
+  `level` int(11) DEFAULT NULL,
+  `roleId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `firstName`, `lastName`, `userName`, `password`, `level`) VALUES
-(1, 'Brad', 'Wooten', 'bwooten','Password1', 11),
-(2, 'Cody', 'Sterup', 'csterup', 'Password12', NULL),
-(3, 'Tyler', 'Stading', 'tstading', 'Password123', NULL),
-(4, 'Glenn', 'Ray', 'gray', 'Password1234', NULL);
+INSERT INTO `user` (`id`, `firstName`, `lastName`, `userName`, `password`, `level`, `roleId`) VALUES
+(1, 'Brad', 'Wooten', 'bwooten', 'aa', 11, 1),
+(2, 'Cody', 'Sterup', 'csterup', 'Password12', NULL, 1),
+(3, 'Tyler', 'Stading', 'tstading', 'Password123', NULL, 1),
+(4, 'Glenn', 'Ray', 'gray', 'Password1234', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -105,10 +124,17 @@ ALTER TABLE `question`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
-ALTER TABLE `user` 
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `roleId` (`roleId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -118,7 +144,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `question`
@@ -127,10 +153,26 @@ ALTER TABLE `question`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fkroleId` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
